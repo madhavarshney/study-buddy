9ea475ecbdae263f2225530b2fb6872a586e6382
Mokhalad Aljuboori <72766064+ogvenocity@users.noreply.github.com>
2022-10-15 23:23:48 -0700

    adding in matierial UI, react-router-dom and some components

---
+++ web-frontend/src/Components/Login/index.jsx
@@ -0,0 +1,11 @@
+
+const Login = () => {
+    const [userName, setUsername] = useState("username");
+    const [password, setPassword] = useState("password");
+    return ( 
+    <>
+        
+    </> );
+}
+ 
+export default Login;
