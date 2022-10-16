c462dced2d0206e06181bc0f630bc18fb3bedc59
Madhav Varshney <madhavarshney@gmail.com>
2022-10-15 18:39:00 -0700

    Set up basic API server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(helmet())
+
+app.get('/', (req, res) => {
+  res.json({ status: 'up' })
+})
+
+app.listen(port, () => {
+  console.log(`Study Buddy API server listening on port ${port}`)
+})
