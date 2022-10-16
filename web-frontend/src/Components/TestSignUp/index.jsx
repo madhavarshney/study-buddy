242bdb9672cea3f13ec47c29cfcf8398d6d9a597
nima64 <kennethe407@gmail.com>
2022-10-16 07:14:57 -0700

    signup blob doesn't shift when resizing

--- web-frontend/src/Components/TestSignUp/index.jsx
+++ web-frontend/src/Components/TestSignUp/index.jsx
@@ -3,6 +3,7 @@ import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import blobSvg from './blob.svg';
 import MyImage from './humaans2.png';
+import { display } from '@mui/system';
 
 
 const TestSignUp = () => {
@@ -16,7 +17,10 @@ const TestSignUp = () => {
           </div>
           <div>
             <h2 style={{ fontSize: "2rem", color: '#119bc0', fontWeight: 800 }} color='#119bc0'>Need Help <br />Finding a Study Group?</h2>
-            <Button variant="contained" style={{ backgroundColor: "#119bc0" }}>Sign Up</Button>
+            <div style={{ width: "6rem", margin: "0 auto", marginBottom: "1rem", display: "flex", flexDirection: 'column' }}>
+              <Button variant="outlined" >Login</Button>
+              <Button variant="contained" style={{ backgroundColor: "#119bc0" }}>Signup</Button>
+            </div>
           </div>
           <img style={{ position: "absolute", width: "40rem", top: "0px", left: "-4rem", zIndex: "-1", opacity: "0.1" }} src={blobSvg} alt="blob image" />
         </div>
