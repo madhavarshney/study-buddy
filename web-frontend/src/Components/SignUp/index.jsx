af5ba5ab910ce5c52997a373b283051de5f29520
joeygaitan <joeygaitan707@gmail.com>
2022-10-16 01:01:24 -0700

    added in login signup

--- web-frontend/src/Components/SignUp/index.jsx
+++ web-frontend/src/Components/SignUp/index.jsx
@@ -1,23 +1,56 @@
-import GoogleLogin from 'react-google-login';
-or
-import { GoogleLogin } from 'react-google-login';
-
-const responseGoogle = (response) => {
-    console.log(response);
-  }
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
 const SignUp = () => {
-    console.log(process.env.REACT_APP_CLIENT_ID)
+    const [ profile, setProfile ] = useState([]);
+    const clientId = process.env.REACT_APP_CLIENT_ID
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
+
+    const onSuccess = (res) => {
+        setProfile(res.profileObj);
+    };
+
+    const onFailure = (err) => {
+        console.log('failed to login :(', err);
+    };
+
+    const logOut = () => {
+        setProfile(null);
+    };
 
     return ( <>
-
-    <GoogleLogin
-        clientId={process.env.REACT_APP_CLIENT_ID}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-    />,
+            <h2>React Google Login</h2>
+            <br />
+            <br />
+            {profile ? (
+                <div>
+                    <img src={profile.imageUrl} alt="user image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+            )}
     </> );
 }
  
